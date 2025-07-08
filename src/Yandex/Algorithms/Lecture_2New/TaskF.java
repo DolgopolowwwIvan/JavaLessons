844a7c3afc814c87c465191fc34ff65c1273b4d0
BillieJEan808 <dolgopolowwwedu@gmail.com>
2025-07-08 18:56:11 +0700

    Сравнять репозитории

--- src/Yandex/Algorithms/Lecture_2New/TaskF.java
+++ src/Yandex/Algorithms/Lecture_2New/TaskF.java
@@ -1,4 +1,32 @@
 package Yandex.Algorithms.Lecture_2New;
 
+import java.util.*;
+
 public class TaskF {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+        List<Integer> array = new LinkedList<>();
+
+        for (int i = 0; i < N; i++) {
+            int value = scanner.nextInt();
+            array.add(i, value);
+        }
+
+        scanner.close();
+
+        for (int i = 0; i < N; i++) {
+            if(!Objects.equals(array.get(i), array.get(N-1)) && (N != 0)){
+                array.add(array.get(i));
+                N -= 1;
+            }else{
+                break;
+            }
+        }
+
+        for (int i = 0; i < array.size(); i++) {
+            System.out.println(array.get(i));
+        }
+
+    }
 }
