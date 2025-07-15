2e3f1d4ce3895c779732e508d0ce1e1d793a4c0b
BillieJEan808 <dolgopolowwwedu@gmail.com>
2025-07-15 13:06:55 +0700

    Починить
    # Conflicts:
    #	src/Yandex/Algorithms/Lecture_2New/TaskG.java
    #	src/Yandex/Algorithms/Lecture_2New/input.txt

--- src/Yandex/Algorithms/Lecture_2New/TaskF.java
+++ src/Yandex/Algorithms/Lecture_2New/TaskF.java
@@ -6,27 +6,50 @@ public class TaskF {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int N = scanner.nextInt();
-        List<Integer> array = new LinkedList<>();
+        int[] array = new int[N];
 
         for (int i = 0; i < N; i++) {
-            int value = scanner.nextInt();
-            array.add(i, value);
+            array[i] = scanner.nextInt();
         }
-
         scanner.close();
 
-        for (int i = 0; i < N; i++) {
-            if(!Objects.equals(array.get(i), array.get(N-1)) && (N != 0)){
-                array.add(array.get(i));
-                N -= 1;
-            }else{
+
+        boolean isSymmetric = true;
+        for (int i = 0; i < N / 2; i++) {
+            if (array[i] != array[N - 1 - i]) {
+                isSymmetric = false;
                 break;
             }
         }
 
-        for (int i = 0; i < array.size(); i++) {
-            System.out.println(array.get(i));
+        if (isSymmetric) {
+            System.out.println(0);
+            return;
         }
 
+
+        for (int i = 0; i < N; i++) {
+            boolean valid = true;
+            for (int j = i, k = N - 1; j <= k; j++, k--) {
+                if (array[j] != array[k]) {
+                    valid = false;
+                    break;
+                }
+            }
+            if (valid) {
+                System.out.println(i);
+                for (int j = i - 1; j >= 0; j--) {
+                    System.out.println(array[j]);
+                }
+                return;
+            }
+        }
+
+
+        System.out.println(N - 1);
+        for (int i = N - 2; i >= 0; i--) {
+            System.out.println(array[i]);
+        }
     }
+
 }
