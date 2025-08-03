7cb1b55e0937d56718fd5c73861cd44d07741749
BillieJEan808 <dolgopolowwwedu@gmail.com>
2025-08-03 16:22:11 +0700

    Бинарные, Бинарного поиска и АВЛ

--- src/Yandex/Algorithms/Lecture_3/TaskB.java
+++ src/Yandex/Algorithms/Lecture_3/TaskB.java
@@ -2,6 +2,7 @@ package Yandex.Algorithms.Lecture_3;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.*;
 
 // Даны два списка чисел, которые могут содержать до 10000 чисел каждый.
@@ -9,32 +10,7 @@ import java.util.*;
 // список в порядке возрастания.
 // Примечание. И даже эту задачу на Питоне можно решить в одну строчку.
 public class TaskB {
-    public static void main(String[] args) throws FileNotFoundException {
-        var scanner = new Scanner(new File("D:\\Java\\practice\\JavaLessons\\src\\Yandex\\Algorithms\\Lecture_2New\\input.txt"));
-        Set<Integer> list1 = new HashSet<>();
-        Set<Integer> list2 = new HashSet<>();
-
-        while (scanner.hasNextLine()) {
-            int input = scanner.nextInt();
-            list1.add(input);
-        }
-
-        while (scanner.hasNextLine()) {
-            int input = scanner.nextInt();
-            list2.add(input);
-        }
-
-        scanner.close();
-
-        int Max = Math.max(list1.size(), list2.size());
-
-        int listNew[] = new int[list1.toArray()];
-        list2.toArray();
-
-        for (int i = 0; i < Max; i++) {
-            if(list1.){
-
-            }
-        }
+    public static void main(String[] args) throws IOException {
+        
     }
 }
